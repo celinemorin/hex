9c4bb62b8aacb5c702ec5c35618eebf8f8c5a888
Krysmo <holmieres@gmail.com>
2016-05-06 21:28:30 +0200

    Added files via upload

---
+++ includes/interfaceJeu.h
@@ -0,0 +1,10 @@
+#ifndef INTERFACEJEU_H
+#define INTERFACEJEU_H
+
+#include "interface.h"
+
+Axe recuperationCellule(Axe position);
+Axe attente_evenement_jeu(SDL_Surface *ecran);
+Axe creation_interface_jeu(int numMenu);
+
+#endif
