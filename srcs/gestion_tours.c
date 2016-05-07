6b1b8629b4fb831e3261a95cd34abee98f336b21
celine <celine.morin.42@gmail.com>
2016-05-07 17:34:31 +0200

    commentaires terminés

--- srcs/gestion_tours.c
+++ srcs/gestion_tours.c
@@ -12,6 +12,13 @@
 #include "gestion_tours.h"
 #include "unionfind.h"
 
+/*	rôle : ajouter deux nodes adjacentes à un même chemin via leurs pointeurs group.
+	Infos infos : la structure à modifier.
+	int X1 : l'abcisse de la node 1.
+	int Y1 : l'ordonnée de la node 1.
+	int X2 : l'abcisse de la node 2.
+	int Y2 : l'abcisse de la node 2. */
+
 static void add_adjacent (Infos infos, int X1, int Y1, int X2, int Y2, State color)
 {
 	if (!(X2 < 1 || X2 > 11 || Y2 < 1 || Y2 > 11 || infos->plateau[X2 - 1][Y2 - 1] != color))
@@ -22,6 +29,13 @@ static void add_adjacent (Infos infos, int X1, int Y1, int X2, int Y2, State col
 			uf_union(infos->blue_root, get_ID(X1, Y1), get_ID(X2, Y2));
 	} 
 }
+
+/*	rôle : fonction de base pour ajouter un pion dans le jeu + vérifier la validité des coups.
+	Infos infos : la structure à modifier.
+	int X : abcisse du pion joué.
+	int Y : ordonnée du pion joué.
+	State color : couleur du joueur qui joue (red ou blue).
+	retour : TRUE si le pion a pu être ajouté, FALSE si sa position n'est pas valide. */
 
 bool add_pion (Infos infos, int X, int Y, State color)
 {
@@ -44,6 +58,11 @@ bool add_pion (Infos infos, int X, int Y, State color)
 	}
 	return (success);
 }
+
+/*	rôle : définir si le joueur 1 ou 2 a gagné en jouant son pion.
+	Infos infos : le jeu en cours à examiner.
+	State color : la couleur du joueur qui vient de jouer.
+	retour : TRUE si le joueur qui vient de jouer a gagné, false si personne n'a gagné. */
 
 bool end_of_game (Infos infos, State color)
 {
