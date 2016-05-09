6b19b02fc9dd8d7cf2fa73cb727898fda9fdb348
Krysmo <holmieres@gmail.com>
2016-05-09 17:07:56 +0200

    Add files via upload

--- includes/interface.h
+++ includes/interface.h
@@ -1,53 +1,15 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
+#ifndef INTERFACEJEU_H
+#define INTERFACEJEU_H
 
-#define HAUTEUR 600 
-#define LARGEUR 800  
+#include "interface.h"
+#include "types.h"
+#include "save.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <stdbool.h>
- 
-#include <SDL/SDL.h>
-#include <SDL/SDL_keysym.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_ttf.h>
-  
-#define POSX 100
-#define POSJOUERY 200
-#define POSCHARGY 300
-#define POSQUITY 450
-
-#define POSHVHY 200
-#define POSHV1Y 300
-#define POSHV2Y 400
-#define POSRETOURY 500
-
-#define POSJEUY 175
-
-#define JOUER 1
-#define CHARGEMENT 2
-#define QUITTER 3
-#define RETOUR 4
-#define NEWGAME 5
-
-typedef struct
-{
-    int x, y;
-}Axe;
-
-typedef struct
-{
-    int h, w;
-}Dimensions;
-
-void chargementUneImage(SDL_Surface *ecran, char * emplacement,int x,int y);
-void initialisationImage(SDL_Surface *ecran, int numeroMenu);
-void clavierPos(SDL_Surface *ecran,int posClavier,int numeroMenu);
-void souriPos(SDL_Surface *ecran,Axe pos,int numeroMenu);
-int attente_evenement_menu(SDL_Surface *ecran, int numeroMenu);
-int creation_interface(SDL_Surface *ecran,int numMenu);
+Axe recuperationCellule(Axe position);
+void placement_pion(SDL_Surface *ecran, Axe position, int couleur);
+void affichageGeneral(SDL_Surface *ecran, Infos varInfo);
+void interfaceHistorique(SDL_Surface *ecran, Infos varInfo);
+Axe attente_evenement_jeu(SDL_Surface *ecran, Infos varInfo, int joueur);
+void creation_interface_jeu(SDL_Surface *ecran,int numMenu);
 
 #endif
---
+++ includes/main.h
@@ -0,0 +1,12 @@
+#ifndef PROJET_H
+#define PROJET_H
+
+#include "interface.h"
+#include "interfaceJeu.h"
+#include "types.h"
+#include "gestion_tours.h"
+#include "memory.h"
+#include "save.h"
+
+
+#endif 
